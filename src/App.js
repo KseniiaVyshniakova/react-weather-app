2515818f7876df1024e93c925be0f72732bc421f
KseniiaVyshniakova <87467695+KseniiaVyshniakova@users.noreply.github.com>
2022-08-03 14:38:08 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://gentle-raindrop-9732e4.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kseniia Vyshniakova
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/KseniiaVyshniakova/react-weather-app"
+          targer="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
