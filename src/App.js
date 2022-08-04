be80a3de74f5b489a645f0bba5867976b5dc929a
KseniiaVyshniakova <87467695+KseniiaVyshniakova@users.noreply.github.com>
2022-08-04 13:26:39 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
@@ -23,6 +24,7 @@ function App() {
           open-sourced on GitHub
         </a>
       </footer>
+      </div>
     </div>
   );
 }
