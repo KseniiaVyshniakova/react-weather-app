c77ed2c8f571bc6124bbbccc2b6b2b3b859331db
KseniiaVyshniakova <87467695+KseniiaVyshniakova@users.noreply.github.com>
2022-08-06 14:33:15 +0300

    Added currently icon and unit conversion

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+       icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
